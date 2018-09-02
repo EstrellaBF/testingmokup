4de7fd4b1890f39bb5c4b066b036afbfee90b6de
EstrellaBF <stern.von.nord@gmail.com>
2018-09-01 22:08:30 -0500

    uploading

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -8,31 +9,31 @@ import { EmployeeService } from '../employee.service';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-	public employees: string;
+  public employees: string;
 
-	getData: string;
+  getData: string;
   postData: string;
-  
+
   key: string = "";
-  values: string ="";
+  values: string = "";
   obj = {};
 
-  get data():string { 
-    return this.employeeService.serviceData; 
-  } 
-  set data(value: string) { 
-    this.employeeService.serviceData = value; 
-  } 
+  get data(): string {
+    return this.employeeService.serviceData;
+  }
+  set data(value: string) {
+    this.employeeService.serviceData = value;
+  }
 
-  constructor(private employeeService: EmployeeService) { 
+  constructor(private httpClient: HttpClient, private employeeService: EmployeeService) {
   }
 
   ngOnInit() {
-  	//this.employees = this.employeeService.getEmployees();
-  	this.employeeService.getEmployees()
-  		.subscribe(data => this.employees  = data, 
-  			error => console.log(error)
-  		);
+    //this.employees = this.employeeService.getEmployees();
+    this.employeeService.getEmployees()
+      .subscribe(data => this.employees = data,
+        error => console.log(error)
+      );
   }
 
 
@@ -40,10 +41,10 @@ export class EmployeeListComponent implements OnInit {
   onTestGet() {
     this.employeeService.getCurrentTime()
       .subscribe(
-          data => this.getData = JSON.stringify(data),
-          error => console.log(error),
-          () => console.log('completed') 
-        );
+        data => this.getData = JSON.stringify(data),
+        error => console.log(error),
+        () => console.log('completed')
+      );
   }
 
   onKey(event: any) { // without type info
@@ -58,24 +59,65 @@ export class EmployeeListComponent implements OnInit {
 
   onTestPost() {
     this.employeeService.postJSON()
-    .subscribe(
-      data => {this.postData = JSON.stringify(data), console.log(this.key), console.log(this.values), console.log(this.postData)},
-      error => console.log(error),
-      () => console.log() 
-    )
+      .subscribe(
+        data => { this.postData = JSON.stringify(data)}, //, console.log(this.key), console.log(this.values), console.log(this.postData) 
+        error => console.log(error),
+        () => console.log()
+      )
   }
 
-  
+
 
   name: string = '';
+  age: number;
+  found: boolean = false;
+  pollo = "pollito";
   onNameKeyUp(event: any) {
     this.name = event.target.value;
   }
 
+  
   getProfile(){
-    console.log(this.name)
+    this.httpClient.get(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles/?name=${this.name}`)
+      .subscribe(
+        (data:any[]) => {
+          console.log(data);
+          if(data.length) {
+            this.age = data[0].age;
+            this.found = true;
+          }
+        }
+      )
   }
 
+/*
+  onProfile() {
+    this.employeeService.getProfile()
+      .subscribe(
+        (data: any[]) => {
+          console.log(data);
+          if(data.length) {
+            this.age = data[0].age;
+            this.found = true;
+          }
+        }
+      )
+  }
+*/
+  //------------------
 
+  
+  postProfile() {
+    this.httpClient.post(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles/`,
+      {
+        name: `${this.key}`,
+        age: 7
+      })
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+        }
+      )
+  }
 
 }
